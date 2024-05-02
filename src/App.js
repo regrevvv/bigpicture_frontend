decb4b5d51e6586c2965bc973ffce694e7d8d35d
regrevvv <txr0317@gmail.com>
2024-05-03 03:40:40 +0900

    Implemented Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import Header from "./Header.js"
+import styles from "./styles/App.module.css"
 
 function App() {
   return (
-    <div id='wrapper'>
-
+    <div id='wrapper' className={styles.wrapper}>
+        <Header></Header>
     </div>
   );
 }
